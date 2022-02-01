be5e9fc773e305b9f73fb1689a06ea4a94706c5a
Hugo Mauricio Morales Acevedo <58606889+Hugo-Morales@users.noreply.github.com>
2022-02-01 09:23:46 -0300

    Relacion entre bd Hecho

--- api/src/db.js
+++ api/src/db.js
@@ -30,10 +30,12 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Pokemon } = sequelize.models;
+const { Country, Activities } = sequelize.models;
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
+Country.belongsToMany(Activities, { through: 'countries_activities', timestamps: false })
+Activities.belongsToMany(Country, { through: 'countries_activities', timestamps: false })
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
