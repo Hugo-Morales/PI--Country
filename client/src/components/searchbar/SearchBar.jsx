348b10e0fecad15ed79640da610685c9c3e7b6e1
Hugo Mauricio Morales Acevedo <58606889+Hugo-Morales@users.noreply.github.com>
2022-02-02 17:26:50 -0300

    Creo el componente search, no funciona

---
+++ client/src/components/searchbar/SearchBar.jsx
@@ -0,0 +1,10 @@
+export default function Navbar() {
+    return (
+        <>
+            <form>
+                <input type="text" />
+                <button type="submit">Buscar</button>
+            </form>
+        </>
+    );
+}
