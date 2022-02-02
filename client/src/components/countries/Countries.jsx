4a607b3a4bab08efa6280cb8c76bacd65f58ed7d
Hugo Mauricio Morales Acevedo <58606889+Hugo-Morales@users.noreply.github.com>
2022-02-02 17:24:54 -0300

    Muestro los datos de db

---
+++ client/src/components/countries/Countries.jsx
@@ -0,0 +1,20 @@
+import { Link } from 'react-router-dom'
+import styles from './Countries.module.css'
+
+export default function Countries({ flag, name, continente, id }) {
+    return (
+        <div className={styles.container}>
+            <div className={styles.title}>
+                <Link to={`/country/${id}`}>
+                    <h3>País: {name}</h3>
+                </Link>
+            </div>
+            <div className={styles.img}>
+                <img src={flag} alt='flag' />
+            </div>
+            <div className={styles.title}>
+                <h4>Continente: {continente}</h4>
+            </div>
+        </div>
+    )
+}
