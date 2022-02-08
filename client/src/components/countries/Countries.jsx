1e33e8b3a67c741efd3777d6fff4586731f0cebf
Hugo Mauricio Morales Acevedo <58606889+Hugo-Morales@users.noreply.github.com>
2022-02-08 11:56:03 -0300

    Added styles on countries

--- client/src/components/countries/Countries.jsx
+++ client/src/components/countries/Countries.jsx
@@ -4,16 +4,18 @@ import styles from './Countries.module.css'
 export default function Countries({ flag, name, continente, id }) {
     return (
         <div className={styles.container}>
-            <div className={styles.title}>
-                <Link to={`/country/${id}`}>
-                    <h3>{name} | ({id})</h3>
-                </Link>
-            </div>
             <div className={styles.img}>
                 <img src={flag} alt='flag' />
             </div>
-            <div className={styles.title}>
-                <h4>Continente: {continente}</h4>
+            <div className={styles.contai_title}>
+                <div className={styles.title}>
+                    <Link to={`/country/${id}`}>
+                        <h3>{name}</h3>
+                    </Link>
+                </div>
+                <div className={styles.title}>
+                    <h4>Continente: {continente}</h4>
+                </div>
             </div>
         </div>
     )
